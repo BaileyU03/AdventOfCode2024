9a35e1f55a5ff6544143ba6c6b187f7631578f98
bpu500 <bailey.uniacke.03@gmail.com>
2024-12-04 12:54:58 +0000

    Day 04 part 02 complete

--- day04.py
+++ day04.py
@@ -67,16 +67,48 @@ def get_xmass(w, x, y):
             + is_northwest(w, x, y)*1 + is_northeast(w, x, y)*1 + is_southwest(w, x, y)*1 + is_southeast(w, x, y)*1)
 
 
-wordsearch = []
-with open("./files/day04.txt", "r") as f:
-    for line in f:
-        wordsearch.append(list(line.split("\n")[0]))
+def is_x_mas(w, x, y):
+    total = 0
+    if w[y-1][x-1] == "M" and w[y+1][x+1] == "S":
+        total += 1
+    elif w[y-1][x-1] == "S" and w[y+1][x+1] == "M":
+        total += 1
+    if w[y-1][x+1] == "M" and w[y+1][x-1] == "S":
+        total += 1
+    elif w[y-1][x+1] == "S" and w[y+1][x-1] == "M":
+        total +=1
+    return total == 2
 
-total = 0
-for y, row in enumerate(wordsearch):
-    for x, char in enumerate(row):
-        if char == "X":
-            total += get_xmass(wordsearch,x,y)
 
-print(total)
+def part01(wordsearch):
+    total = 0
+    for y, row in enumerate(wordsearch):
+        for x, char in enumerate(row):
+            if char == "X":
+                total += get_xmass(wordsearch, x, y)
+    return total
 
+
+def part02(wordsearch):
+    total = 0
+    for y, row in enumerate(wordsearch):
+        if y == 0 or y == len(wordsearch) - 1:
+            continue
+        for x, char in enumerate(row):
+            if x == 0 or x == len(wordsearch[y]) - 1:
+                continue
+            if char == "A":
+                total += is_x_mas(wordsearch, x, y)*1
+    return total
+
+
+def main():
+    wordsearch = []
+    with open("./files/day04.txt", "r") as f:
+        for line in f:
+            wordsearch.append(list(line.split("\n")[0]))
+    print(part01(wordsearch))
+    print(part02(wordsearch))
+
+
+main()
