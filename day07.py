24698399d175276c429c7520f04b8e02c9cd1f03
bpu500 <bailey.uniacke.03@gmail.com>
2024-12-07 12:44:30 +0000

    Day 07 part 01 complete

--- day07.py
+++ day07.py
@@ -1,9 +1,6 @@
 def is_valid_rule(rule):
     if len(rule) == 2:
-        if rule[0] == rule[1]:
-            return True
-        else:
-            return False
+        return rule[0] == rule[1]
     rule_after_add = [rule[0] - rule[-1]] + rule[1:-1]
     if rule[0] % rule[-1] == 0:
         rule_after_mul = [rule[0] // rule[-1]] + rule[1:-1]
@@ -11,16 +8,33 @@ def is_valid_rule(rule):
     return is_valid_rule(rule_after_add)
 
 
+def is_valid_rule_part2(rule):
+    # more naive :( but only runs if there's no solution w/o concatenation
+    if len(rule) == 2:
+        return rule[0] == rule[1]
+    rule_after_add = [rule[0]] + [rule[1] + rule[2]] + rule[3:]
+    rule_after_mul = [rule[0]] + [rule[1] * rule[2]] + rule[3:]
+    rule_after_con = [rule[0]] + [concatenate(rule[1], rule[2])] + rule[3:]
+    return is_valid_rule_part2(rule_after_add) or is_valid_rule_part2(rule_after_mul) or is_valid_rule_part2(rule_after_con)
+
+
+def concatenate(a, b):
+    return int(str(a) + str(b))
+
+
 rules = []
 with open("files/day07.txt", "r") as f:
     for line in f:
         rules.append(list(map(int, line.replace(":", "").split())))
 
-
-total = 0
+total1 = 0
+total2 = 0
 for rule in rules:
     if is_valid_rule(rule[:]):
-        total += rule[0]
+        total1 += rule[0]
+    elif is_valid_rule_part2(rule[:]):
+        total2 += rule[0]
 
+total2 += total1
 
-print(total)
+print(total1, total2)
