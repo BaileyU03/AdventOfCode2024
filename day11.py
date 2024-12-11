6e92fde8548c9acfb1ca65e369ca1c6906fcb7e4
bpu500 <bailey.uniacke.03@gmail.com>
2024-12-11 12:59:21 +0000

    Day 11 part 02 complete

--- day11.py
+++ day11.py
@@ -1,18 +1,26 @@
+from functools import cache
+
 with open("files/day11.txt", "r") as f:
     line = list(map(int, f.readline().split("\n")[0].split()))
 
-for i in range(25):
-    print(i)
-    new_line = []
-    for rock in line:
-        s_rock = str(rock)
-        if rock == 0:
-            new_line.append(1)
-        elif len(s_rock) % 2 == 0:
-            new_line.append(int(s_rock[:len(s_rock)//2]))
-            new_line.append(int(s_rock[len(s_rock)//2:]))
-        else:
-            new_line.append(rock*2024)
-    line = new_line
 
-print(len(line))
+@cache
+def x(num, depth, limit):
+    if depth == limit:
+        return 1
+    s_num = str(num)
+    if num == 0:
+        return x(1, depth+1, limit)
+    if len(s_num) % 2 == 0:
+        return x(int(s_num[:len(s_num)//2]), depth+1, limit) + x(int(s_num[len(s_num)//2:]), depth+1, limit)
+    else:
+        return x(num*2024, depth+1, limit)
+
+
+total1 = 0
+total2 = 0
+for rock in line:
+    total1 += x(rock, 0, 25)
+    total2 += x(rock, 0, 75)
+print(total1)
+print(total2)
