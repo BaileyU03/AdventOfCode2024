5bd14cdbf8d0bfe089f9cc8157c49f62c7fd2f84
bpu500 <bailey.uniacke.03@gmail.com>
2024-12-11 13:02:54 +0000

    Day 11 tweaks

--- day11.py
+++ day11.py
@@ -1,21 +1,20 @@
 from functools import cache
 
-with open("files/day11.txt", "r") as f:
-    line = list(map(int, f.readline().split("\n")[0].split()))
-
 
 @cache
 def x(num, depth, limit):
+    s_num = str(num)
     if depth == limit:
         return 1
-    s_num = str(num)
     if num == 0:
         return x(1, depth+1, limit)
     if len(s_num) % 2 == 0:
         return x(int(s_num[:len(s_num)//2]), depth+1, limit) + x(int(s_num[len(s_num)//2:]), depth+1, limit)
-    else:
-        return x(num*2024, depth+1, limit)
+    return x(num*2024, depth+1, limit)
 
+
+with open("files/day11.txt", "r") as f:
+    line = list(map(int, f.readline().split("\n")[0].split()))
 
 total1 = 0
 total2 = 0
