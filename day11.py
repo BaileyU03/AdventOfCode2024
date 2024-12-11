fa0246e2d86f1566e61a075984d7fdecfc69ff3e
bpu500 <bailey.uniacke.03@gmail.com>
2024-12-11 13:04:30 +0000

    Day 11 rename bad function name

--- day11.py
+++ day11.py
@@ -2,15 +2,16 @@ from functools import cache
 
 
 @cache
-def x(num, depth, limit):
+def count(num, depth, limit):
     s_num = str(num)
     if depth == limit:
         return 1
     if num == 0:
-        return x(1, depth+1, limit)
+        return count(1, depth + 1, limit)
     if len(s_num) % 2 == 0:
-        return x(int(s_num[:len(s_num)//2]), depth+1, limit) + x(int(s_num[len(s_num)//2:]), depth+1, limit)
-    return x(num*2024, depth+1, limit)
+        return (count(int(s_num[:len(s_num) // 2]), depth + 1, limit)
+              + count(int(s_num[len(s_num) // 2:]), depth + 1, limit))
+    return count(num * 2024, depth + 1, limit)
 
 
 with open("files/day11.txt", "r") as f:
@@ -19,7 +20,7 @@ with open("files/day11.txt", "r") as f:
 total1 = 0
 total2 = 0
 for rock in line:
-    total1 += x(rock, 0, 25)
-    total2 += x(rock, 0, 75)
+    total1 += count(rock, 0, 25)
+    total2 += count(rock, 0, 75)
 print(total1)
 print(total2)
