f4c6484db964ace9202a237903fbbdc83012afe7
bpu500 <bailey.uniacke.03@gmail.com>
2024-12-13 14:23:11 +0000

    Day 13 part 02 complete

--- day13.py
+++ day13.py
@@ -10,7 +10,7 @@ with open("files/day13.txt", "r") as f:
             rule = {}
             continue
         new_line = line.split("\n")[0].split(": ")[1].split(", ")
-        xy = np.array([int(new_line[0][2:]), int(new_line[1][2:])])
+        xy = [int(new_line[0][2:]), int(new_line[1][2:])]
         if "A" in line:
             rule["A"] = xy
         elif "B" in line:
@@ -20,23 +20,21 @@ with open("files/day13.txt", "r") as f:
     rules.append(rule)
 
 
-total = 0
+total1 = 0
+total2 = 0
 for rule in rules:
-    min_tokens = -1
-    A = rule["A"]
-    B = rule["B"]
-    P = rule["P"]
-    a = min(rule["P"] // rule["A"])
-    b = 0
-    while a >= 0:
-        while all(a*A + b*B < P):
-            b += 1
-        if all(a*A + b*B == P):
-            if min_tokens == -1:
-                min_tokens = 3*a + b
-            else:
-                min_tokens = min(min_tokens, 3*a + b)
-        a -= 1
-    total += min_tokens if min_tokens > -1 else 0
+    a_mat = np.array([rule["A"], rule["B"]]).T
+    # Part 1
+    solution = np.linalg.solve(a_mat, np.array(rule["P"]))
+    rounded = np.round(solution)
+    if all(rounded == np.round(solution, 3)):       # Very rough way of checking for int
+        total1 += solution @ np.array([3, 1])
+    # Part 2
+    new_p = np.array(list(map(lambda x: x + 10000000000000, rule["P"])))
+    solution2 = np.linalg.solve(a_mat, new_p)
+    rounded = np.round(solution2)
+    if all(rounded == np.round(solution2, 3)):
+        total2 += solution2 @ np.array([3, 1])
 
-print(total)
+print(int(total1))
+print(int(total2))
