a266d6ec6dd0d46c1f0acc7d09ec1881cf2a9971
bpu500 <bailey.uniacke.03@gmail.com>
2024-12-16 10:24:10 +0000

    Day 16 part 01 complete

---
+++ day16.py
@@ -0,0 +1,59 @@
+maze = []
+with open("files/day16.txt", "r") as f:
+    for y, line in enumerate(f):
+        ll = list(line.split("\n")[0])
+        maze.append(ll)
+
+
+all_vertices = []
+for y, line in enumerate(maze):
+    for x, val in enumerate(line):
+        if val == ".":
+            all_vertices.append({"pos": (x, y), "dist": float("inf"), "prev": (), "dir": (), "type": 0})
+        elif val == "S":
+            all_vertices.append({"pos": (x, y), "dist": 0, "prev": (), "dir": (1, 0), "type": 1})
+        elif val == "E":
+            all_vertices.append({"pos": (x, y), "dist": float("inf"), "prev": (), "dir": (), "type": 2})
+
+
+vertices = all_vertices[:]
+while len(vertices) > 0:
+    poss = [v["pos"] for v in vertices]
+    dists = [v['dist'] for v in vertices]
+    u = vertices[min(range(len(dists)), key=dists.__getitem__)]
+    vertices.remove(u)
+    # Right
+    if (u["pos"][0] + 1, u["pos"][1]) in poss:
+        v = vertices[poss.index((u["pos"][0] + 1, u["pos"][1]))]
+        alt = u["dist"] + 1 if u["dir"] == (1, 0) else u["dist"] + 1001
+        if alt < v["dist"]:
+            v["dist"] = alt
+            v["prev"] = u["pos"]
+            v["dir"] = (1, 0)
+    # Left
+    if (u["pos"][0] - 1, u["pos"][1]) in poss:
+        v = vertices[poss.index((u["pos"][0] - 1, u["pos"][1]))]
+        alt = u["dist"] + 1 if u["dir"] == (-1, 0) else u["dist"] + 1001
+        if alt < v["dist"]:
+            v["dist"] = alt
+            v["prev"] = u["pos"]
+            v["dir"] = (-1, 0)
+    # Up
+    if (u["pos"][0], u["pos"][1] - 1) in poss:
+        v = vertices[poss.index((u["pos"][0], u["pos"][1] - 1))]
+        alt = u["dist"] + 1 if u["dir"] == (0, -1) else u["dist"] + 1001
+        if alt < v["dist"]:
+            v["dist"] = alt
+            v["prev"] = u["pos"]
+            v["dir"] = (0, -1)
+    # Down
+    if (u["pos"][0], u["pos"][1] + 1) in poss:
+        v = vertices[poss.index((u["pos"][0], u["pos"][1] + 1))]
+        alt = u["dist"] + 1 if u["dir"] == (0, 1) else u["dist"] + 1001
+        if alt < v["dist"]:
+            v["dist"] = alt
+            v["prev"] = u["pos"]
+            v["dir"] = (0, 1)
+
+
+print([v["dist"] for v in all_vertices if v["type"] == 2][0])
