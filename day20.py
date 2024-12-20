3192aa4be231106e274b3f41e7e0cf44d6756678
bpu500 <bailey.uniacke.03@gmail.com>
2024-12-20 13:10:43 +0000

    Day 20 part 02 complete

--- day20.py
+++ day20.py
@@ -6,15 +6,16 @@ with open("files/day20.txt", "r") as f:
             start = (line.index("S") + 1, y + 1)
         if "E" in line:
             end = (line.index("E") + 1, y + 1)
-
 pad_line = [["#" for _ in range(len(track[0]))]]
 track = pad_line + track + pad_line
 
 
+pos_dict = {}
 counter = 0
 current_pos = start
 while current_pos != end:
     track[current_pos[1]][current_pos[0]] = counter
+    pos_dict[counter] = current_pos
     if track[current_pos[1] + 1][current_pos[0]] in [".", "E"]:
         current_pos = (current_pos[0], current_pos[1] + 1)
     elif track[current_pos[1] - 1][current_pos[0]] in [".", "E"]:
@@ -25,9 +26,11 @@ while current_pos != end:
         current_pos = (current_pos[0] - 1, current_pos[1])
     counter += 1
 track[current_pos[1]][current_pos[0]] = counter
+pos_dict[counter] = current_pos
 
 
 MIN_CHEAT = 100
+# Part 01
 total = 0
 for y, line in enumerate(track):
     for x, val in enumerate(line):
@@ -44,6 +47,18 @@ for y, line in enumerate(track):
             if diff >= MIN_CHEAT:
                 total += 1
 
+print(total)
+
+
+# Part 02
+total = 0
+for cheat_length in range(MIN_CHEAT, len(pos_dict.keys())):
+    for dist in range(cheat_length, len(pos_dict.keys())):
+        pos1 = pos_dict[dist]
+        pos2 = pos_dict[dist - cheat_length]
+        diff = abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+        if diff <= 20 and cheat_length - diff >= MIN_CHEAT:
+            total += 1
 print(total)
 
 
