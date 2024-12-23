b34e900aa9f4ab3a124459d68e660bb65d3fbef9
bpu500 <bailey.uniacke.03@gmail.com>
2024-12-23 13:54:32 +0000

    Day 23 part 02 complete

--- day23.py
+++ day23.py
@@ -12,6 +12,7 @@ with open("files/day23.txt", "r") as f:
             adj_list[split_line[1]] = [split_line[0]]
 
 
+# Part 01
 tris = []
 ts = [n for n in adj_list if n[0] == "t"]
 visited_ts = []     # for ignoring duplicates
@@ -29,4 +30,27 @@ for t in ts:
             if b in adj_list[a]:
                 tris.append((t, a, b))
 
-print(len(tris))
+
+# Part 02
+biggest_cliques = [[k] for k in adj_list.keys()]
+next_clique_size = [[]]
+while len(next_clique_size) > 0:
+    next_clique_size = []
+    for clique in biggest_cliques:
+        for new_v in adj_list.keys():
+            if new_v in clique:
+                continue
+            adj_to_all = True
+            for v in clique:
+                if new_v not in adj_list[v]:
+                    adj_to_all = False
+                    break
+            if adj_to_all:
+                new_clique = clique + [new_v]
+                new_clique.sort()
+                if new_clique not in next_clique_size:
+                    next_clique_size.append(new_clique)
+    if len(next_clique_size) > 0:
+        biggest_cliques = next_clique_size[:]
+
+print(",".join(biggest_cliques[0]))
